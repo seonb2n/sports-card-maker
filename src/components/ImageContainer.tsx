447b3b735241b9a7a212a249e725c3a55f60eb83
seonb2n <626ksb@gmail.com>
2024-02-26 22:30:08 +0900

    default image 를 보여주도록 변경

---
+++ src/components/ImageContainer.tsx
@@ -0,0 +1,37 @@
+
+type ImageContainerProps = {
+    imgFile?: File[],
+}
+
+export const ImageContainer = ({imgFile}: ImageContainerProps) => {
+
+    const ExampleImageContainer = () => {
+        return (
+            <div>
+                <img
+                    src="/assets/img/example.jpg"
+                    alt="Description of my image"
+                    width={200}
+                    height={300}
+                />
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            {imgFile === undefined ?
+                <ExampleImageContainer/>
+                : (imgFile.map((file, index) => (
+                        <img
+                            key={index}
+                            src={URL.createObjectURL(file)}
+                            alt={`Uploaded Image ${index + 1}`}
+                            style={{width: '200px', height: 'auto', marginBottom: '10px'}}
+                        />
+                    ))
+                )
+            }
+        </div>
+    );
+}
--- src/components/InputDataForm.tsx
+++ src/components/InputDataForm.tsx
@@ -2,28 +2,22 @@
 
 import Dropzone from "@/components/Dropzone";
 import {useState} from "react";
+import {ImageContainer} from "@/components/ImageContainer";
 
 export const InputDataForm = () => {
     const [droppedFile, setDroppedFile] = useState<File[]>();
 
-    console.log(droppedFile[0]?.name);
-
     return (
         <div>
             <div>
-                <Dropzone onDrop={(files: File[]) => setDroppedFile(files)} />
                 <div>
-                    <h2>Uploaded Images</h2>
+                    <h2>Generated Images</h2>
                     <div>
-                        {droppedFile.map((file, index) => (
-                            <img
-                                key={index}
-                                src={URL.createObjectURL(file)}
-                                alt={`Uploaded Image ${index + 1}`}
-                                style={{ width: '200px', height: 'auto', marginBottom: '10px' }}
-                            />
-                        ))}
+                        <ImageContainer imgFile={droppedFile} />
                     </div>
+                </div>
+                <div>
+                    <Dropzone onDrop={(files: File[]) => setDroppedFile(files)} />
                 </div>
             </div>
         </div>
