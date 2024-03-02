b316080fa7a97715d26e1afee237189ea71ab583
seonb2n <626ksb@gmail.com>
2024-03-02 16:44:32 +0900

    feature: 이미지 필드 클립으로 업로드할 수 있는 기능 추가

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -18,10 +18,30 @@ const Dropzone: React.FC<DropzoneProps> = ({onDrop}) => {
         e.preventDefault();
     }, []);
 
+    const handleClick = () => {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = '*/*';
+        fileInput.multiple = true;
+        fileInput.style.display = 'none';
+
+        fileInput.addEventListener('change', (e: Event) => {
+            const target = e.target as HTMLInputElement;
+            if (target.files) {
+                const files = Array.from(target.files);
+                onDrop(files);
+            }
+            fileInput.remove();
+        });
+        document.body.appendChild(fileInput);
+        fileInput.click();
+    };
+
     return (
         <div
             onDrop={handleDrop}
             onDragOver={handleDragOver}
+            onClick={handleClick}
             style={{
                 width: '100%',
                 height: '100px',
@@ -30,6 +50,7 @@ const Dropzone: React.FC<DropzoneProps> = ({onDrop}) => {
                 justifyContent: 'center',
                 alignItems: 'center',
                 backgroundColor: 'rgb(213,213,213)',
+                cursor: 'pointer',
             }}
         >
             Drop files here
